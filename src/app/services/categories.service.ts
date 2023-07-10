d874c01a482a01ca5aa32ddb5236b4d93aa74cd1
AbishekRosarioJS <abishekrosario@gmail.com>
2023-07-11 00:31:44 +0530

    works

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor(private storage:AngularFireStorage ,private afs :AngularFirestore,private toastr :ToastrService) { }
+  constructor(
+    private storage:AngularFireStorage,
+    private service :AngularFirestore,
+    private toastr :ToastrService,
+    private router: Router,
+    ) { }
 
    uploadImage(selectedImage:any,postData:any){
     const filePath = `postIMG/${Date.now()}`;
@@ -30,9 +37,25 @@ export class PostsService {
    }
   
    saveData(postData:any){
-    this.afs.collection('posts').add(postData).then(docRef => {
+    this.service.collection('posts').add(postData).then(docRef => {
       this.toastr.success('Data Insert Successfully');
+      this.router.navigate(['/posts']);
     });
    }
+
+
+   loadData() {
+    return  this.service.collection('posts').snapshotChanges().pipe(
+       map(actions => {
+        return actions.map(a => {
+           const data = a.payload.doc.data();
+           const id = a.payload.doc.id;
+           return {id, data}
+ 
+         })
+       })
+     )
+   }
+   
 
 }
